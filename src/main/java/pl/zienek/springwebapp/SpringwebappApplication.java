f3449bd5ec74e5148eef73905ce1765e4179e08c
zzienek <bazibart@gmail.com>
2018-01-08 18:39:17 +0100

    Initial commit

---
+++ src/main/java/pl/zienek/springwebapp/SpringwebappApplication.java
@@ -0,0 +1,12 @@
+package pl.zienek.springwebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringwebappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringwebappApplication.class, args);
+	}
+}
---
+++ src/test/java/pl/zienek/springwebapp/SpringwebappApplicationTests.java
@@ -0,0 +1,16 @@
+package pl.zienek.springwebapp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringwebappApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
